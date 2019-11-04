3ede28f6e440cd5595b69e4f0fb2d4b1013a2d51
Gema Muhammad Reza <gema@skyshi.com>
2019-11-04 16:08:49 +0700

    change row

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,13 +6,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {
+            !this.props.dev && (<link rel="stylesheet" href={`${this.props.__NEXT_DATA__.assetPrefix}/static/style.css`} />)
+          }
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <link rel="stylesheet" href="/static/style.css" />
           <link href="https://fonts.googleapis.com/css?family=Oswald:400,500|Source+Serif+Pro|ZCOOL+XiaoWei&display=swap" rel="stylesheet"></link>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
-          {
-            !this.props.dev && (<link rel="stylesheet" href={`${this.props.__NEXT_DATA__.assetPrefix}/static/style.css`} />)
-          }
         </Head>
         <body>
           <div className="container">
