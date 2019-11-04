7286954340ada832066371c12399e54275227a22
Gema Muhammad Reza <gema@skyshi.com>
2019-11-04 16:16:49 +0700

    change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {
-            !this.props.dev && (<link rel="stylesheet" href={`${this.props.__NEXT_DATA__.assetPrefix}/static/style.css`} />)
+            !this.props.dev && (<link rel="stylesheet" href={`${this.props.__NEXT_DATA__.assetPrefix}/_next/static/css/style.css`} />)
           }
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <link rel="stylesheet" href="/static/style.css" />
