d9feabf9948df34645c27f4ea23e826d6c9f88b3
Gema Muhammad Reza <gema@skyshi.com>
2019-11-14 15:56:05 +0700

    semangat

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,10 +6,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="stylesheet" href="https://codepen.io/gemamreza/pen/Exxdveq.css" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,500|Source+Serif+Pro|ZCOOL+XiaoWei&display=swap"></link>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
-          <link rel="stylesheet" href="https://codepen.io/gemamreza/pen/Exxdveq.css" />
         </Head>
         <body>
           <div className="container">
